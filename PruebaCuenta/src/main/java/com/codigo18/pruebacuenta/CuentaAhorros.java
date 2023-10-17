c64299d111ccb86da22b682ae89b279cca9e522d
Valentina Rodriguez Henao <144085605+valtinarh@users.noreply.github.com>
2023-10-16 23:22:07 -0500

    Add files via upload

---
+++ PruebaCuenta/src/main/java/com/codigo18/pruebacuenta/CuentaAhorros.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codigo18.pruebacuenta;
+
+/**
+ *
+ * @author varoh
+ */
+class CuentaAhorros extends Cuenta{
+    private boolean activa;
+    
+
+    public CuentaAhorros(float saldo, float tasa) {
+        super(saldo, tasa);
+        activa = saldo >= 10000;
+
+    }
+    public void retirar(float cantidad) {
+        if (activa) {
+            super.retirar(cantidad);
+        }
+    }
+
+    public void consignar(float cantidad) {
+        if (activa) {
+            super.consignar(cantidad);
+        }
+    }
+    public void extractoMensual() {
+
+        if (numeroRetiros > 4) {
+            comisionMensual += (numeroRetiros - 4) * 1000;
+        }
+        super.extractoMensual();
+        if ( saldo < 10000 ) {
+            activa = false;
+        }
+    }
+
+    public void imprimir() {
+        System.out.println("Saldo = $" + saldo);
+        System.out.println("Comisión mensual = $" + comisionMensual);
+        System.out.println("Número de transacciones = " + (numeroConsignaciones + numeroRetiros));
+        System.out.println();
+    }
+    
+}
---
+++ PruebaCuenta/src/main/java/com/codigo18/pruebacuenta/CuentaCorriente.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codigo18.pruebacuenta;
+
+/**
+ *
+ * @author varoh
+ */
+public class CuentaCorriente extends Cuenta {
+    float sobregiro;
+
+    public CuentaCorriente(float saldo, float tasa)
+    {
+        super(saldo, tasa);
+        sobregiro = 0;
+    }
+    @Override
+    public void retirar ( float cantidad)
+    {
+        float resultado = saldo - cantidad;
+        if (resultado < 0)
+        {
+            sobregiro = sobregiro - resultado;
+            saldo = 0;
+        }
+        else
+        {
+            super.retirar(cantidad);
+        }
+    }
+
+    @Override
+    public void consignar ( float cantidad)
+    {
+        float residuo = sobregiro - cantidad;
+        if (sobregiro > 0)
+        {
+            if ( residuo > 0)
+            {
+                sobregiro = 0;
+                saldo = residuo;
+            }
+            else
+            {
+                sobregiro = -residuo;
+                saldo = 0;
+            }
+        }
+        else
+        {
+            super.consignar(cantidad);
+        }
+
+    }
+    @Override
+    public void extractoMensual()
+    {
+        super.extractoMensual();
+    }
+
+    public void imprimir()
+    {
+        System.out.println("Saldo = $ " + saldo);
+        System.out.println("Cargo mensual = $ " + comisionMensual);
+        System.out.println("Número de transacciones = " + (numeroConsignaciones + numeroRetiros));
+        System.out.println("Valor de sogregiro = $" + (numeroConsignaciones + numeroRetiros));
+        System.out.println();
+    }
+    
+}
